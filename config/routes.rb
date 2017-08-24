e6909b5b2d8234a498b0705f748b88e8d1bf8f55
Andrew Neidley <neidz44@gmail.com>
2017-08-24 02:11:28 -0500

    Checkpoint 4 - User Profile: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   get 'welcome/index'
 
   get 'welcome/about'
 
-  root 'welcome#index'
+  root 'users#show'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
