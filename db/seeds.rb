8bad10594db2009f9d8159ce85f685b9f1e4fb08
Andrew Neidley <neidz44@gmail.com>
2017-08-25 00:46:28 -0500

    Checkpoint 6 - Seeding Data: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: 'drewdawg44@hotmail.com',
+  password: 'helloworld',
+  username: 'Neidley'
+)
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    username: Faker::Zelda.character,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
+
+10.times do
+  Item.create!(
+    user: users.sample,
+    name: Faker::Beer.name
+  )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
