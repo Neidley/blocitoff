5c5492bf4833b126dd2561636deba32c948907db
Andrew Neidley <neidz44@gmail.com>
2017-09-28 12:00:55 -0500

    working with cookies

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,49 +1,49 @@
-class ItemsController < ApplicationController
+ class ItemsController < ApplicationController
 
-  def new
-    @item = Item.new
-  end
+   def new
+     @item = Item.new
+   end
 
-  def index
-    @items = Item.all
-    render json: @items
-  end
+   def index
+     @items = Item.all
+     render json: @items
+   end
 
-  def create
-    @item = Item.new
-    @item.name = params[:item][:name]
-    @item.user = current_user || User.first
-    @item.completed = false
+   def create
+     @item = Item.new
+     @item.name = params[:item][:name]
+     @item.user = current_user
+     @item.completed = false
 
-    if @item.save!
-      flash[:notice] = "Item was saved."
-      redirect_to root_path
-    else
-      flash.now[:alert] = "There was an error saving the item. Please try again."
-      render :new
-    end
-  end
+     if @item.save!
+       flash[:notice] = "Item was saved."
+       render json: @item
+     else
+       flash.now[:alert] = "There was an error saving the item. Please try again."
+       render json: @item.errors
+     end
+   end
 
-  def update
-    @item = Item.find(params[:id])
-    @item.completed = params[:completed]
+   def update
+     @item = Item.find(params[:id])
+     @item.completed = params[:completed]
 
-    if @item.save
-      render json: @item
-    else
-      render json: @item.errors
-    end
-  end
+     if @item.save
+       render json: @item
+     else
+       render json: @item.errors
+     end
+   end
 
-  def destroy
-    @item = Item.find(params[:id])
+   def destroy
+     @item = Item.find(params[:id])
 
-    if @item.destroy
-      flash[:notice] = "\"#{@item.name}\" was deleted successfully."
-      redirect_to root_path
-    else
-      flash.now[:alert] = "There was an error deleting the item."
-      render root_path
-    end
-  end
-end
+     if @item.destroy
+       flash[:notice] = "\"#{@item.name}\" was deleted successfully."
+       redirect_to root_path
+     else
+       flash.now[:alert] = "There was an error deleting the item."
+       render root_path
+     end
+   end
+ end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   # before_action :authenticate_user!
 
   def show
-    @user = User.first
+    @user = User.find(params[:id])
     @items = @user.items
     render json: @items
   end
